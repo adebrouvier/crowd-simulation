b5596ec96c27607d4b9cd33e131b9792b8a122ae
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2018-07-11 22:05:08 -0300

    Sliding window

--- sliding_window.m
+++ sliding_window.m
@@ -1,24 +1,10 @@
-function sliding_window(input_file, dt, window_size, output_file)
+function sliding_window(input_file, window_size, output_file)
   data = csvread(input_file);
-  times = [0:dt:max(data) + 2 *dt];
-  events = zeros(1, columns(times));
-  i = 1;
-  for k = data'
-    while k > times(i+1)
-      i += 1;
-      if i + 1 >= columns(times)
-        break;
-      endif
-    endwhile
-    if i + 1 >= columns(times)
-      break;
-    endif
-    events(i) += 1;
-  endfor
+  particles = [1:rows(data)];
   i = 1;
   window_arr = [];
-  while i + window_size - 1 < columns(events)
-    window_arr = [window_arr; times(i), times(i+window_size), sum(events(i:i+window_size - 1))];
+  while i + window_size - 1 < columns(particles)
+    window_arr = [window_arr; particles(i), particles(i+window_size - 1), (data(i+window_size - 1) - data(i)) ];
     i += 1;
   endwhile
   % save output_file window_arr
