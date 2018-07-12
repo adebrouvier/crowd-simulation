c14a79e21f7c725f16e17c75909df8b5f1148413
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2018-07-12 20:47:16 -0300

    Fix sliding window

--- sliding_window.m
+++ sliding_window.m
@@ -4,7 +4,7 @@ function sliding_window(input_file, window_size, output_file)
   i = 1;
   window_arr = [];
   while i + window_size - 1 < columns(particles)
-    window_arr = [window_arr; particles(i), particles(i+window_size - 1), (data(i+window_size - 1) - data(i)) ];
+    window_arr = [window_arr; particles(i), particles(i+window_size - 1), window_size / (data(i+window_size - 1) - data(i)) ];
     i += 1;
   endwhile
   % save output_file window_arr
